2042c74d0f71063aca2d7392a2803309da84f8e1
bakp22 <101152896+bakp22@users.noreply.github.com>
2024-07-14 00:15:36 -0400

    profile info added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import logo from "./me.png"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="App-content">
+        <p className="name">Beren Akpinar</p>
+        <p className='d1'> Junior undergraduate studying Computer Science and Neuroscience <br></br> 
+                                    at Boston University</p>
+
+         <p className='d2'>
+            Interests: neurotechnology, robotics, software development, healthcare
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      
+        
+      </div>
+        
+      <img width="300" height="300" src={logo} alt="Logo" />
+
+        <ul className="skills-list">
+          <li>☕ Java</li>
+          <li>🐍 Python</li>
+          <li>⚛ React</li>
+          <li>📝 JavaScript</li>
+          <li>🕸️ HTML/CSS</li>
+        </ul>
+
+        <button className='Github'>GitHub</button>
+
+
       </header>
+
+   
+
+      
+
+ 
+
     </div>
   );
 }
